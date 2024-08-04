cf979e3af160f1892bc451b73d7d71afc05adba9
Nostea <kiwiuchi@googlemail.com>
2024-08-04 20:25:15 +0200

    fixed not loading svg in landing page

--- src/Pages/Landingpage/Landingpage.jsx
+++ src/Pages/Landingpage/Landingpage.jsx
@@ -2,18 +2,20 @@ import { Link } from "react-router-dom";
 import "./Landingpage.css";
 
 const Landingpage = () => {
-    return (
-        <section className="landing-container">
-            <div className="landing-food">
-                <img src="./public/foodillustration.svg" alt="FLYING FOOD" />
-            </div>
-            <div className="landing-box">
-                <h2 className="landing-title">All recipe you needed</h2>
-                <p className="landing-text">5000+ healthy recipes made by people for a healthy life</p>
-                <button className="landing-btn"><Link to="/home">Get Started</Link></button>
-            </div>
-        </section>
-    );
-}
- 
+  return (
+    <section className="landing-container">
+      <div className="landing-food">
+        <img src="/foodillustration.svg" alt="FLYING FOOD" />
+      </div>
+      <div className="landing-box">
+        <h2 className="landing-title">All recipe you needed</h2>
+        <p className="landing-text">5000+ healthy recipes made by people for a healthy life</p>
+        <button className="landing-btn">
+          <Link to="/home">Get Started</Link>
+        </button>
+      </div>
+    </section>
+  );
+};
+
 export default Landingpage;
